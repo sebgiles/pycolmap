fe5ad9fd5d093dc4b7cff059b1c5c111c50daca9
Sebastian Giles <sgiles@ethz.ch>
2021-06-12 15:02:17 +0200

    Make sequence localization work

--- main.cc
+++ main.cc
@@ -12,8 +12,8 @@ namespace py = pybind11;
 #include "sift.cc"
 
 PYBIND11_MODULE(pycolmap, m) {
-    m.doc() = "COLMAP plugin built at " __TIMESTAMP__;
-    
+    m.doc() = "COLMAP plugin built on " __TIMESTAMP__;
+
     // Absolute pose.
     m.def("absolute_pose_estimation", &absolute_pose_estimation,
           py::arg("points2D"), py::arg("points3D"),
@@ -31,6 +31,8 @@ PYBIND11_MODULE(pycolmap, m) {
             py::arg("rel0_points2D_1"),
             py::arg("camera_dict"),
             py::arg("max_error_px") = 12.0,
+            py::arg("rel_max_error_px") = 12.0,
+            py::arg("rel_weight") = 1000.0,
             "Absolute pose estimation with non-linear refinement.");
 
     // Absolute pose from multiple images.
@@ -41,7 +43,6 @@ PYBIND11_MODULE(pycolmap, m) {
           py::arg("camera_dicts"),
           py::arg("max_error_px") = 12.0,
           "Multi image absolute pose estimation.");
-
 
     // Essential matrix.
     m.def("essential_matrix_estimation", &essential_matrix_estimation,
@@ -56,7 +57,7 @@ PYBIND11_MODULE(pycolmap, m) {
           py::arg("points2D1"), py::arg("points2D2"),
           py::arg("max_error_px") = 4.0,
           "LORANSAC + 7-point algorithm.");
-    
+
     // Image-to-world and world-to-image.
     m.def("image_to_world", &image_to_world, "Image to world transformation.");
     m.def("world_to_image", &world_to_image, "World to image transformation.");
